7ef5b64e90901c49c6c8a0bbc890e646d24f3b23
Larva Team <support@larva.com.cn>
2021-03-07 00:09:16 +0800

    init

---
+++ src/DingTalkServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This is NOT a freeware, use is subject to license terms
+ * @copyright Copyright (c) 2010-2099 Jinan Larva Information Technology Co., Ltd.
+ * @link http://www.larva.com.cn/
+ * @license http://www.larva.com.cn/license/
+ */
+
+namespace Larva\DingTalk;
+
+use Illuminate\Support\ServiceProvider;
+
+class DingTalkServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+
+        }
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
